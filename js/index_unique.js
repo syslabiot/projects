c8d6952be7f3cf415b2ed6998660ff7323b20e70
techpool <suryadeep10@gmail.com>
2017-03-18 08:15:27 +0530

    auto https redirection added

--- js/index_unique.js
+++ js/index_unique.js
@@ -1,15 +1,15 @@
 var vehicle1 = document.getElementById("Vehicle1");
-var vehicle1Pic= document.getElementById("Vehicle1-ID");
+var vehicle1Pic = document.getElementById("Vehicle1-ID");
 var vehicle2 = document.getElementById("Vehicle2");
-var vehicle2Pic= document.getElementById("Vehicle2-ID")
+var vehicle2Pic = document.getElementById("Vehicle2-ID")
 var astronaut1 = document.getElementById("Astronaut1");
-var astronaut1Pic= document.getElementById("Astronaut1-ID")
+var astronaut1Pic = document.getElementById("Astronaut1-ID")
 var astronaut2 = document.getElementById("Astronaut2");
-var astronaut1Pic= document.getElementById("Astronaut2-ID")
+var astronaut1Pic = document.getElementById("Astronaut2-ID")
 var physicist1 = document.getElementById("Physicist1");
-var physicist1Pic= document.getElementById("Physicist1-ID")
+var physicist1Pic = document.getElementById("Physicist1-ID")
 var physicist2 = document.getElementById("Physicist2");
-var physicist2Pic= document.getElementById("Physicist2-ID")
+var physicist2Pic = document.getElementById("Physicist2-ID")
 vehicle1.addEventListener("mouseenter", fetch);
 vehicle2.addEventListener("mouseenter", fetch);
 astronaut1.addEventListener("mouseenter", fetch);
@@ -18,15 +18,19 @@ physicist1.addEventListener("mouseenter", fetch);
 physicist2.addEventListener("mouseenter", fetch);
 
 function fetch() {
- var Images = document.querySelectorAll("img");
- // for (var i = 0; i < Images.length; i++) {
- // 	Images[i].className = "hide";
- // }
- var PicId = this.attributes["data-img"].value;
- var Pic = document.getElementById(PicId);
- if (Pic.className === "hide") {
- 	Pic.className = "";
- } else {
- 	Pic.className = "hide";
- }
+    var Images = document.querySelectorAll("img");
+    // for (var i = 0; i < Images.length; i++) {
+    // 	Images[i].className = "hide";
+    // }
+    var PicId = this.attributes["data-img"].value;
+    var Pic = document.getElementById(PicId);
+    if (Pic.className === "hide") {
+        Pic.className = "";
+    } else {
+        Pic.className = "hide";
+    }
+}
+
+if (location.protocol != 'https:') {
+    location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
 }
